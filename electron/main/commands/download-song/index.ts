bc9baec98598aa7487bf5e6d715891183b340ccc
Wesley Lowe <wesley.lowe@yahoo.com>
2022-12-31 14:35:48 -0600

    added utility methods to delete records and song files

--- electron/main/commands/download-song/index.ts
+++ electron/main/commands/download-song/index.ts
@@ -1,36 +1,5 @@
-import axios from "axios";
-import ytdl from "ytdl-core";
-import fs from "fs";
-import { v4 as uuidv4 } from "uuid";
-import * as downloadFunctions from "./functions";
-
 import { songManager } from "../../services/song-manager";
 
 export const downloadSong = async (url: string) => {
-    if (!ytdl.validateURL(url)) return null;
-
-    const songExists = await songManager.checkObstruction(url)
-    if (songExists) return null;
-
-    const songInfo = await downloadFunctions.getInfo(url);
-    const thumbnailUrl = songInfo.thumbnail.url;
-    const id = uuidv4();
-
-    let [audio, thumbnail] = await Promise.all([
-        downloadFunctions.downloadSong(url),
-        downloadFunctions.downloadThumbnail(thumbnailUrl)
-    ]);
-
-    await Promise.allSettled([
-        audio.pipe(fs.createWriteStream(`./songs/audio/${id}.mp3`)),
-        thumbnail.pipe(fs.createWriteStream(`./songs/thumbnails/${id}.png`))
-    ])
-
-    await songManager.storeSongRecord({
-        title: songInfo.title,
-        artist: songInfo.artist,
-        date: songInfo.date,
-        url: url
-    })
+    return songManager.downloadSong(url);
 };
-
--- electron/main/services/song-manager/index.ts
+++ electron/main/services/song-manager/index.ts
@@ -1,6 +1,10 @@
 import fs from "fs";
+import ytdl from "ytdl-core";
+import { v4 as uuidv4 } from "uuid";
+import * as downloadFunctions from "./functions";
 
 type SongType = {
+    id: string,
     title: string,
     artist: string,
     date: string,
@@ -20,22 +24,66 @@ class SongManager {
         //?initialize storage location if non-existent
     };
 
-    public async checkObstruction(url: string) {
+    public async downloadSong(url: string) {
+        if (!ytdl.validateURL(url)) return null;
+
+        const songExists = await songManager.checkObstruction(url)
+        if (songExists) return null;
+    
+        const songInfo = await downloadFunctions.getInfo(url);
+        const thumbnailUrl = songInfo.thumbnail.url;
+        const id = uuidv4();
+    
+        let [audio, thumbnail] = await Promise.all([
+            downloadFunctions.downloadSong(url),
+            downloadFunctions.downloadThumbnail(thumbnailUrl)
+        ]);
+    
+        await Promise.allSettled([
+            audio.pipe(fs.createWriteStream(`./songs/audio/${id}.mp3`)),
+            thumbnail.pipe(fs.createWriteStream(`./songs/thumbnails/${id}.png`))
+        ])
+    
+        await this.storeSongRecord({
+            id: id,
+            title: songInfo.title,
+            artist: songInfo.artist,
+            date: songInfo.date,
+            url: url
+        })
+    };
+
+    public async removeSong(id: string) {
+        await this.removeSongRecord(id);
+
+        return Promise.allSettled([
+            fs.unlinkSync(`./songs/audio/${id}.mp3`),
+            fs.unlinkSync(`./songs/thumbnails/${id}.png`)
+        ])
+    };
+
+    private async checkObstruction(url: string) {
         const songReconds = await this.readJSON();
         const songExists = songReconds.songs.find(song => song.url == url);
 
         return songExists ? true : false;
-    }
-
-    public async downloadSong() {
-
     };
 
-    public async storeSongRecord(newSong: SongType) {
+    private async storeSongRecord(newSong: SongType) {
         let newData = await this.readJSON()
 
         newData.songs.push(newSong);
         this.writeJSON(newData);
+    };
+
+    private async removeSongRecord(id: string) {
+        let newData = await this.readJSON();
+        let songIdx = newData.songs.findIndex(song => song.id);
+
+        if (songIdx >= 0) {
+            newData.songs.splice(songIdx, 1);
+            this.writeJSON(newData);
+        };
     };
 
     private async readJSON() {
