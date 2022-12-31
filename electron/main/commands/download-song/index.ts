025796d88cd2677a4067d52a1e85df5f2bdcfd11
Wesley Lowe <wesley.lowe@yahoo.com>
2022-12-31 14:23:16 -0600

    moved song download functions to a song manager service

--- electron/main/commands/download-song/index.ts
+++ electron/main/commands/download-song/index.ts
@@ -4,12 +4,17 @@ import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 import * as downloadFunctions from "./functions";
 
+import { songManager } from "../../services/song-manager";
+
 export const downloadSong = async (url: string) => {
     if (!ytdl.validateURL(url)) return null;
 
-    const id = uuidv4();
+    const songExists = await songManager.checkObstruction(url)
+    if (songExists) return null;
+
     const songInfo = await downloadFunctions.getInfo(url);
     const thumbnailUrl = songInfo.thumbnail.url;
+    const id = uuidv4();
 
     let [audio, thumbnail] = await Promise.all([
         downloadFunctions.downloadSong(url),
@@ -20,5 +25,12 @@ export const downloadSong = async (url: string) => {
         audio.pipe(fs.createWriteStream(`./songs/audio/${id}.mp3`)),
         thumbnail.pipe(fs.createWriteStream(`./songs/thumbnails/${id}.png`))
     ])
+
+    await songManager.storeSongRecord({
+        title: songInfo.title,
+        artist: songInfo.artist,
+        date: songInfo.date,
+        url: url
+    })
 };
 
--- electron/main/services/song-manager/index.ts
+++ electron/main/services/song-manager/index.ts
@@ -1,11 +1,53 @@
 import fs from "fs";
 
-export class SongManager {
+type SongType = {
+    title: string,
+    artist: string,
+    date: string,
+    url: string
+};
+
+type SongsStorage = {
+    songs: [
+        SongType
+    ]
+};
+
+class SongManager {
+    private readonly recordPath = "./songs/songs.json";
+
     constructor() {
+        //?initialize storage location if non-existent
+    };
+
+    public async checkObstruction(url: string) {
+        const songReconds = await this.readJSON();
+        const songExists = songReconds.songs.find(song => song.url == url);
+
+        return songExists ? true : false;
+    }
+
+    public async downloadSong() {
 
     };
 
-    async storeSong() {
+    public async storeSongRecord(newSong: SongType) {
+        let newData = await this.readJSON()
 
-    }
+        newData.songs.push(newSong);
+        this.writeJSON(newData);
+    };
+
+    private async readJSON() {
+        const data = await fs.readFileSync("./songs/songs.json", "utf8");
+        const jsonData = JSON.parse(data) as SongsStorage;
+
+        return jsonData;
+    };
+
+    private async writeJSON(data: any) {
+        return await fs.writeFileSync(this.recordPath, JSON.stringify(data))
+    };
 }
+
+export const songManager = new SongManager();
